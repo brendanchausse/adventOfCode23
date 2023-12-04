0ef65c60a7fc1b5a85a6f2abc9299fdf8f5a6c5a
brendanchausseChanged <brendan@changedapps.com>
2023-12-04 07:46:44 -0600

    Day 1 Advent of Code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
+import kotlin.system.exitProcess
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+
+    print(findCalibrationValue(input))
+
+}
+
+fun findCalibrationValue(input: List<String>): Int{
+
+    var calibrationNumber = 0
+    var numHolder = ""
+    for(line in input){
+        numHolder = makeNumString(line)
+        if(numHolder.length == 1){
+           val newNumber = (numHolder + numHolder).toInt()
+            calibrationNumber += newNumber
+        }else{
+            val newNumber = (numHolder.substring(0,1) + numHolder.substring(numHolder.length-1)).toInt()
+            calibrationNumber += newNumber
+        }
+
+    }
+
+    return calibrationNumber
+}
+
+fun makeNumString(line: String): String {
+    var numberString = ""
+    for(chr in line){
+        if(chr.isDigit()){
+            numberString += chr
+        }
+    }
+    return numberString
 }
